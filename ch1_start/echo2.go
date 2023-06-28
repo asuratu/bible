a2fbf3b15f8b481a61a1c0cf0c6abb5d0ed17345
AsuraTu <tugecn@gmail.com>
2023-06-28 09:27:52 +0800

    update

--- ch1_start/echo2.go
+++ ch1_start/echo2.go
@@ -11,27 +11,20 @@ func main() {
 	var isPass bool
 	fmt.Println("请输入姓名：")
 	if _, err := fmt.Scanln(&name); err != nil {
-		handleErr(err)
+		fmt.Println("姓名输入有误，请重新输入")
 	}
 	fmt.Println("请输入年龄：")
 	if _, err := fmt.Scanln(&age); err != nil {
-		handleErr(err)
+		fmt.Println("年龄输入有误，请重新输入")
 	}
 	fmt.Println("请输入薪水：")
 	if _, err := fmt.Scanln(&sal); err != nil {
-		handleErr(err)
+		fmt.Println("薪水输入有误，请重新输入")
 	}
 	fmt.Println("请输入是否通过考试：")
 	if _, err := fmt.Scanln(&isPass); err != nil {
-		handleErr(err)
+		fmt.Println("是否通过考试输入有误，请重新输入")
 	}
 	fmt.Printf("名字是：%v\n年龄是：%v\n薪水是：%v\n是否通过考试：%v\n", name, age, sal, isPass)
 
-}
-
-// 处理错误
-func handleErr(err error) {
-	if err != nil {
-		fmt.Println("err=", err)
-	}
 }
